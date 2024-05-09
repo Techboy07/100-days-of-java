d3aa1df9041cf7c44efa83643ce0174065ed0c8a
Toluwalase <technogodwr@gmail.com>
2024-05-09 10:43:19 +0100

    objects as param

--- Main.java
+++ Main.java
@@ -1,58 +1,89 @@
-// java
-class Main {
+class Test {
+    int a, b;
+
+    Test(int i, int j) {
+        a = i;
+        b = j;
+    }
+
+    boolean equals(Test o) {
+        if (o.a == a && o.b == b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class PassOb {
+
     public static void main(String args[]) {
-        Stack mystack1 = new Stack();
-        Stack mystack2 = new Stack();
+        Test ob1 = new Test(100, 22);
+        Test ob2 = new Test(100, 22);
+        Test ob3 = new Test(-1, -1);
 
-        // push some numbers onto the stack
-        for (int i = 0; i < 10; i++)
-            mystack1.push(i);
-        for (int i = 10; i < 20; i++)
-            mystack2.push(i);
-
-        // pop those numbers off the stack
-        System.out.println("Stack in mystack1:");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(mystack1.pop());
-
-        }
-
-        System.out.println("Stack in mystack2:");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(mystack2.pop());
-        }
+        System.out.println("ob1 == ob2: " + ob1.equals(ob2));
+        System.out.println("ob1 == ob3: " + ob1.equals(ob3));
 
     }
 }
 
-class Stack {
-    int stck[] = new int[10];
-    // top of stack
-    int tos;
+class Box {
+    double width;
+    double height;
+    double depth;
 
-    // initialise top of stack
-    Stack() {
-        tos = -1;
+    // Notice this constructor. It takes an object of type Box.
+    Box(Box ob) { // pass object to constructor
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
     }
 
-    // Push an item onto the stack
-    void push(int item) {
-        if (tos == 9) {
-            System.out.println("Stack is full.");
-        } else {
-            stck[++tos] = item;
-        }
+    // constructor used when all dimensions specified
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
     }
 
-    // Pop an item from the stack
-    int pop() {
-        if (tos < 0) {
-            System.out.println("Stack underflow.");
-            return 0;
-        } else {
-            return stck[tos--];
-
-        }
+    // constructor used when no dimensions specified
+    Box() {
+        width = -1; // use -1 to indicate
+        height = -1; // an uninitialized
+        depth = -1; // box
     }
 
+    // constructor used when cube is created
+    Box(double len) {
+        width = height = depth = len;
+    }
+
+    // compute and return volume
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+class OverloadCons2 {
+    public static void main(String args[]) {
+        // create boxes using the various constructors
+        Box mybox1 = new Box(10, 20, 15);
+        Box mybox2 = new Box();
+        Box mycube = new Box(7);
+        Box myclone = new Box(mybox1); // create copy of mybox1
+        double vol;
+        // get volume of first box
+        vol = mybox1.volume();
+        System.out.println("Volume of mybox1 is " + vol);
+        // get volume of second box
+        vol = mybox2.volume();
+        System.out.println("Volume of mybox2 is " + vol);
+        // get volume of cube
+        vol = mycube.volume();
+        System.out.println("Volume of cube is " + vol);
+        // get volume of clone
+        vol = myclone.volume();
+        System.out.println("Volume of clone is " + vol);
+    }
 }
