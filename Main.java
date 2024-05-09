934d1d3032f590a15186ba553fa3c79702431b26
Toluwalase <technogodwr@gmail.com>
2024-05-09 18:08:19 +0100

    stings innerclass stack

--- Main.java
+++ Main.java
@@ -1,18 +1,56 @@
-class Factorial {
-    static int fact(int n) {
-        int result;
+/*  public access specifier 
+ a public member can be accessed by any code 
+ */
 
-        if (n == 1)
-            return 1;
-        result = fact(n - 1) * n;
-        return result;
+// private access specifier
+/*
+ When a member of a class is specified as private, 
+then that member can only be accessed by other members of its class
+ * 
+ */
+
+// default access specifier
+/*
+ When no access specifier is used, 
+ then by default the member of a class is public within its own package,
+  but cannot be accessed outside of its package.
+ */
+
+//  other acces specifiers include:
+//   protected ,abstract and final.
+
+class Test {
+    int a; // default access
+    public int b; // public access
+    private int c; // private access
+
+    // methods to access c
+    void setc(int i) {
+        // set c's value
+        c = i;
+    }
+
+    int getc() {
+        // get c's value
+        return c;
     }
 }
 
-class Recursion {
-    public static void main(String[] args) {
-        System.out.println("Factorial of 3 is " + Factorial.fact(3));
-        System.out.println("Factorial of 4 is " + Factorial.fact(4));
-        System.out.println("Factorial of 5 is " + Factorial.fact(5));
+class AccesTest {
+    public static void main(String args[]) {
+        Test ob = new Test();
+
+        // These are OK, a and b may be accessed directly
+        ob.a = 10;
+        ob.b = 20;
+
+        // This is not OK and will cause an error
+        // ob.c = 100;
+        // Error!
+
+        // You must access c through its methods
+        ob.setc(100); // OK
+        System.out.println("a, b, and c: " + ob.a + " " + ob.b + " " + ob.getc());
+
     }
 }
---
+++ Recursion.java
@@ -0,0 +1,18 @@
+class Factorial {
+    static int fact(int n) {
+        int result;
+
+        if (n == 1)
+            return 1;
+        result = fact(n - 1) * n;
+        return result;
+    }
+}
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println("Factorial of 3 is " + Factorial.fact(3));
+        System.out.println("Factorial of 4 is " + Factorial.fact(4));
+        System.out.println("Factorial of 5 is " + Factorial.fact(5));
+    }
+}
