215ceacac636aa0d075f17ad4e0fb157dd74f92a
Toluwalase <technogodwr@gmail.com>
2024-05-09 14:38:21 +0100

    recursion

--- Main.java
+++ Main.java
@@ -1,89 +1,18 @@
-class Test {
-    int a, b;
+class Factorial {
+    static int fact(int n) {
+        int result;
 
-    Test(int i, int j) {
-        a = i;
-        b = j;
-    }
-
-    boolean equals(Test o) {
-        if (o.a == a && o.b == b) {
-            return true;
-        } else {
-            return false;
-        }
+        if (n == 1)
+            return 1;
+        result = fact(n - 1) * n;
+        return result;
     }
 }
 
-class PassOb {
-
-    public static void main(String args[]) {
-        Test ob1 = new Test(100, 22);
-        Test ob2 = new Test(100, 22);
-        Test ob3 = new Test(-1, -1);
-
-        System.out.println("ob1 == ob2: " + ob1.equals(ob2));
-        System.out.println("ob1 == ob3: " + ob1.equals(ob3));
-
-    }
-}
-
-class Box {
-    double width;
-    double height;
-    double depth;
-
-    // Notice this constructor. It takes an object of type Box.
-    Box(Box ob) { // pass object to constructor
-        width = ob.width;
-        height = ob.height;
-        depth = ob.depth;
-    }
-
-    // constructor used when all dimensions specified
-    Box(double w, double h, double d) {
-        width = w;
-        height = h;
-        depth = d;
-    }
-
-    // constructor used when no dimensions specified
-    Box() {
-        width = -1; // use -1 to indicate
-        height = -1; // an uninitialized
-        depth = -1; // box
-    }
-
-    // constructor used when cube is created
-    Box(double len) {
-        width = height = depth = len;
-    }
-
-    // compute and return volume
-    double volume() {
-        return width * height * depth;
-    }
-}
-
-class OverloadCons2 {
-    public static void main(String args[]) {
-        // create boxes using the various constructors
-        Box mybox1 = new Box(10, 20, 15);
-        Box mybox2 = new Box();
-        Box mycube = new Box(7);
-        Box myclone = new Box(mybox1); // create copy of mybox1
-        double vol;
-        // get volume of first box
-        vol = mybox1.volume();
-        System.out.println("Volume of mybox1 is " + vol);
-        // get volume of second box
-        vol = mybox2.volume();
-        System.out.println("Volume of mybox2 is " + vol);
-        // get volume of cube
-        vol = mycube.volume();
-        System.out.println("Volume of cube is " + vol);
-        // get volume of clone
-        vol = myclone.volume();
-        System.out.println("Volume of clone is " + vol);
+class Recursion {
+    public static void main(String[] args) {
+        System.out.println("Factorial of 3 is " + Factorial.fact(3));
+        System.out.println("Factorial of 4 is " + Factorial.fact(4));
+        System.out.println("Factorial of 5 is " + Factorial.fact(5));
     }
 }
