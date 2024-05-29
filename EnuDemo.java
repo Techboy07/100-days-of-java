6db6fc6ac957f6824a2cb71fc5e1d2ff78a92c6c
Toluwalase <technogodwr@gmail.com>
2024-05-29 20:11:01 +0100

    enums

---
+++ EnuDemo.java
@@ -0,0 +1,36 @@
+
+enum Apple {
+    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+}
+
+class EnuDemo {
+    public static void main(String[] args) {
+        // Apple ap = Apple.RedDel;
+
+        // if (ap == Apple.GoldenDel)
+        // System.out.println("ap contains GoldenDel.\n");
+        // else
+        // System.out.println("not found");
+
+        // // Use an enum to control a switch statement.
+        // switch (ap) {
+        // case Jonathan:
+        // System.out.println("Jonathan is red.");
+        // break;
+        // case GoldenDel:
+        // System.out.println("Golden Delicious is yellow.");
+        // break;
+        // case RedDel:
+        // System.out.println("Red Delicious is red.");
+        // break;
+        // case Winesap:
+        // System.out.println("Winesap is red.");
+        // break;
+        // case Cortland:
+        // System.out.println("Cortland is red.");
+        // break;
+        // }
+
+        System.out.println(Apple.RedDel);
+    }
+}
